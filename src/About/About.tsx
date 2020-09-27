b4bda9588484ba6be4e19bc28db3a822c818773f
Sumit Chaudhari <sumit@cryptorai.com>
2020-09-27 22:17:08 +0800

    fix: eslint

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-export const About = () => (
+const About = () => (
   <div>
     This is About component. <h3>Hello, testing about component</h3>
     <a href="/">Home Page</a>
   </div>
 );
+
+export default About;
