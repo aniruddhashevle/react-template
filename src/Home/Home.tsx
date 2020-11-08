63fb6511ab879328155fc07a0a2f34463afaad8c
sumit-nf <68223214+sumit-nf@users.noreply.github.com>
2020-11-08 17:46:55 +0800

    feat: material ui added

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 import "./Home.css";
 
 const Home = () => (
   <div className="App">
     This is demo component. <h3>Hello, testing testing 123</h3>
     <a href="/about">About Page</a>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
   </div>
 );
 
